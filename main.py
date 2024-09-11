ad832c9ec36c293cbbad97f83a7c9ec1ce13396c
shimizu-gh <r.shimizu+github@youmemiru.co.jp>
2024-09-11 13:58:42 +0000

    a

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pyxel
 
-pyxel.init(200, 150)
-pyxel.cls(8)
-pyxel.line(20, 20, 180, 130, 7)
-pyxel.show()
+
+def __init__(self):
+        pyxel.init(160,120)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+    
+def update(self):
+        self.x = (self.x + 1) % pyxel.width
+    
+def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+
